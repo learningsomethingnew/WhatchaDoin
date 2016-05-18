7e13ef5d64efeb75e94365ed5992ef145eff63e6
Nic Colley <nic.colley@live.com>
2016-05-18 12:09:32 -0400

    changes to statics

--- whatchadoin/whatchadoin/settings.py
+++ whatchadoin/whatchadoin/settings.py
@@ -150,6 +150,6 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    # os.path.join(PROJECT_ROOT, 'static'),
 )
 
